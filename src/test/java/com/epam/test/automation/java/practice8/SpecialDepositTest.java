8052c078f1f1bab7ae3db30257b7a281acea2e64
dmytrokurilov <dmytrokurilov@gmail.com>
2021-02-26 17:21:12 +0200

    Task Done commented tests

--- src/test/java/com/epam/test/automation/java/practice8/SpecialDepositTest.java
+++ src/test/java/com/epam/test/automation/java/practice8/SpecialDepositTest.java
@@ -1,5 +1,5 @@
-package com.epam.test.automation.java.practice8;
+/*package com.epam.test.automation.java.practice8;
 
 public class SpecialDepositTest {
-    //TODO: Delete this line and write your own solution;
-}
+
+}*/
